8fa7ff13c0b8a52a64df5e9048ef0c57abcf7777
Michael Solati <mkslt04@gmail.com>
2018-10-26 02:49:07 -0400

    chore: update for Angular 7.x.x

--- src/ngmeta.module.ts
+++ src/ngmeta.module.ts
@@ -3,16 +3,15 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { NgMetaService } from './ngmeta.service'
 
 @NgModule({
-  imports: [],
   providers: [NgMetaService],
-  declarations: []
 })
 export class NgMetaModule {
   constructor(private _ngmeta: NgMetaService) { }
 
   static forRoot(): ModuleWithProviders {
     return {
-      ngModule: NgMetaModule
+      ngModule: NgMetaModule,
+      providers: [NgMetaService]
     };
   }
 }
