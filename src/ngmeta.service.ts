c392efbeca56aeb3b97734ea40f02161540ef43d
Michael Solati <mkslt04@gmail.com>
2020-05-16 15:53:04 -0700

    refactor: rewrite library and update for angular 9

--- src/ngmeta.service.ts
+++ src/ngmeta.service.ts
@@ -1,146 +1,177 @@
-import { DOCUMENT } from '@angular/common';
 import { Injectable, Inject } from '@angular/core';
-import { ɵgetDOM, ɵDomAdapter } from '@angular/platform-browser';
-import { Router, NavigationEnd, RouterEvent } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { Title, Meta } from '@angular/platform-browser';
 
-import { TagData, MetaData } from './ngmeta.types';
+import { AllMeta, FacebookMeta, GoogleMeta, TwitterMeta } from './ngmeta.types';
 
 /**
 * Service that allows setting and updating of meta tags, title tags, and canonical tags.
 */
 @Injectable()
 export class NgMetaService {
-  private _dom: ɵDomAdapter = ɵgetDOM();
-  private _scroll = true;
+  private _scroll = false;
+
+  constructor(
+    private _titleService: Title,
+    private _metaService: Meta,
+    @Inject(DOCUMENT) private _dom: any
+  ) { }
 
   /**
-  * Initializes service. Creates faux DOM component to abstractly interact with DOM. Subscribes to route events.
-  */
-  constructor(@Inject(DOCUMENT) private _document: any, private _router: Router) {
-    this._router.events.subscribe((evt: RouterEvent) => {
-      this._scrollToTop(evt);
+   * Sets all meta details for page.
+   * @param param0 
+   */
+  setAll({
+    title,
+    description,
+    image,
+    canonical = this._dom.URL,
+    twitter
+  }:
+  AllMeta
+  ): void {
+    this.setTitle(title);
+    this.setDescription(description);
+    this.setFacebook({ title, description, image, url: canonical });
+    this.setGoogle({ title, description, image });
+    this.setTwitter({ title, description, image, site: twitter });
+    this.setCanonical(canonical);
+    this._scrollToTop();
+  }
+
+  /**
+   * Sets canonical URL of web page.
+   * @param canonical The canonical URL for your page.
+   */
+  setCanonical(canonical: string = this._dom.URL): void {
+    let element: HTMLElement = this._dom.querySelector("link[rel=\'canonical\']");
+
+    if (!element) {
+      element = this._dom.createElement('link');
+      element.setAttribute('rel', 'canonical');
+      this._dom.head.appendChild(element);
+    }
+
+    element.setAttribute('href', canonical);
+  }
+
+  /**
+   * Sets description of web page.
+   * @param description Description relating to the content of page.
+   */
+  setDescription(description: string): void {
+    const name = 'description';
+    const elements = this._metaService.getTags(`name="${name}"`);
+    if (elements.length) {
+      this._metaService.updateTag({ name: 'description', content: description }, `name="${name}"`);
+    } else {
+      this._metaService.addTag({ name: 'description', content: description });
+    }
+  }
+
+  /**
+   * Sets Open Graph tags for optimal display on Facebook.
+   * @param param0 
+   */
+  setFacebook(
+    { locale, type, title, description, image, url = this._dom.URL, appId }:
+    FacebookMeta
+  ): void {
+    const fields = [
+      { property: 'og:locale', content: locale },
+      { property: 'og:type', content: type },
+      { property: 'og:title', content: title },
+      { property: 'og:description', content: description },
+      { property: 'og:image', content: image },
+      { property: 'og:url', content: url },
+      { property: 'fb:app_id', content: appId }
+    ];
+
+    fields.forEach((field) => {
+      if (field.content) {
+        const elements = this._metaService.getTags(`property="${field.property}"`);
+        if (elements.length) {
+          this._metaService.updateTag({ property: field.property, content: field.content }, `property="${field.property}"`);
+        } else {
+          this._metaService.updateTag({ property: field.property, content: field.content });
+        }
+      }
     });
   }
 
   /**
-  * Sets canonical tag for page, to call `this._ngmeta.canonical = 'https://www.google.com';`.
-  * @param {string} canonicalURL Canonical URL for the page.
-  */
-  set canonical(canonicalURL: string) {
-    try {
-      this._removeTag('[rel=\'canonical\']');
-      const canonical: HTMLElement = this._dom.createElement('link');
-      this._dom.setAttribute(canonical, 'rel', 'canonical');
-      this._dom.setAttribute(canonical, 'href', canonicalURL);
-      this._dom.appendChild(this._document.head, canonical);
-    } catch (e) { }
+   * Sets microdata tags for optimal display on Google.
+   * @param param0 
+   */
+  setGoogle(
+    { title, description, image }:
+    GoogleMeta
+  ): void {
+    const fields = [
+      { itemprop: 'title', content: title },
+      { itemprop: 'description', content: description },
+      { itemprop: 'image', content: image }
+    ];
+
+    fields.forEach((field) => {
+      if (field.content) {
+        const elements = this._metaService.getTags(`itemprop="${field.itemprop}"`);
+        if (elements.length) {
+          this._metaService.updateTag({ itemprop: field.itemprop, content: field.content }, `itemprop="${field.itemprop}"`);
+        } else {
+          this._metaService.updateTag({ itemprop: field.itemprop, content: field.content });
+        }
+      }
+    });
   }
 
   /**
-  * Returns string value of current page's title, to call `let title: string = this._ngmeta.title;`.
-  * @return {string} Current page's title.
-  */
-  get title(): string {
-    try {
-      return this._dom.getTitle(this._document);
-    } catch (e) { return ''; }
-  }
-
-  /**
-  * Sets `<title></title>` tag for page, to call `this._ngmeta.title = 'Google';`.
-  * @param title Title value for the page.
-  */
-  set title(title: string) {
-    try {
-      this._dom.setTitle(this._document, title);
-    } catch (e) { }
-  }
-
-  /**
-  * Returns boolean value if scroll to top is enabled, to call `let scroll: boolean = this._ngmeta.scroll;`.
-  * @returns If scroll to top is enabled.
-  */
-  get scroll(): boolean {
-    return this._scroll;
-  }
-
-  /**
-  * Sets wether `<body></body>` should scroll to top on route change, to call `this._ngmeta.scroll = true;`.
-  * @param scroll If `<body></body>` should scroll.
-  */
-  set scroll(scroll: boolean) {
+   * Sets if after `setAll` window will scroll to top.
+   * @param scroll Boolean of wether to scroll to top or not.
+   */
+  setScroll(scroll: boolean): void {
     this._scroll = scroll;
   }
 
   /**
-  * Creates HTML for a `<meta>` tag of any attribute.
-  * @param metaData The attribute (like 'name' or 'property') type (like 'description' or 'og:title') and content of the tag.
-  */
-  public createMeta(metaData: MetaData): void {
-    try {
-      if (typeof metaData.attribute === 'string' && typeof metaData.type === 'string' && typeof metaData.content === 'string') {
-        this._removeTag(`[${metaData.attribute}='${metaData.type}']`);
-        const meta: HTMLElement = this._dom.createElement('meta');
-        this._dom.setAttribute(meta, metaData.attribute, metaData.type);
-        this._dom.setAttribute(meta, 'content', metaData.content);
-        this._dom.appendChild(this._document.head, meta);
-      }
-    } catch (e) { }
+   * Sets document's title shown in a browser's title bar or a page's tab.
+   * @param title Document's title.
+   */
+  setTitle(title: string): void {
+    this._titleService.setTitle(title);
   }
 
   /**
-  * Set function setting all `<head></head>` metadata.
-  * @param tagData An object of the new values for the tags an user wants.
-  */
-  public setHead(tagData: TagData): void {
-    try {
-      // Edit <title>
-      if (typeof tagData.title === 'string') {
-        this.title = tagData.title;
-      }
-      // Edit meta tags with Name attribute
-      if (tagData.name instanceof Array) {
-        for (let detail of tagData.name) {
-          detail.attribute = 'name';
-          this.createMeta(detail);
+   * Sets tags for optimal display as a Twitter card.
+   * @param param0 
+   */
+  setTwitter(
+    { title, description, image, alt, site }:
+    TwitterMeta
+  ): void {
+    const fields = [
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:title', content: title },
+      { name: 'twitter:description', content: description },
+      { name: 'twitter:image', content: image },
+      { name: 'twitter:image:alt', content: (image && alt) || title },
+      { name: 'twitter:site', content: site }
+    ];
+
+    fields.forEach((field) => {
+      if (field.content) {
+        const elements = this._metaService.getTags(`name="${field.name}"`);
+        if (elements.length) {
+          this._metaService.updateTag({ name: field.name, content: field.content }, `name="${field.name}"`);
+        } else {
+          this._metaService.updateTag({ name: field.name, content: field.content });
         }
       }
-      // Edit meta tags with Property attribute
-      if (tagData.property instanceof Array) {
-        for (let detail of tagData.property) {
-          detail.attribute = 'property';
-          this.createMeta(detail);
-        }
-      }
-      // Edit meta tags with any attribute
-      if (tagData.meta instanceof Array) {
-        for (let detail of tagData.meta) {
-          this.createMeta(detail);
-        }
-      }
-      // Edit canonical tag
-      if (typeof tagData.canonical === 'string') {
-        this.canonical = tagData.canonical;
-      }
-    } catch (e) { }
+    });
   }
 
-  /**
-  * Removes HTML from `<head></head>` for a tag.
-  * @param tagSelector Selector detail for tag to remove.
-  */
-  private _removeTag(tagSelector: string): void {
-    try {
-      const tag: HTMLElement = this._dom.querySelector(this._document.head, tagSelector);
-      this._dom.remove(tag);
-    } catch (e) { }
-  }
-
-  /**
-  * Scrolls web page to top of `<body></body>`.
-  */
-  private _scrollToTop(evt: RouterEvent): void {
-    if ((typeof window !== 'undefined') && (evt instanceof NavigationEnd) && !evt.url.includes('#')) {
+  private _scrollToTop(): void {
+    if (this._scroll && (typeof window !== 'undefined') && !window.location.pathname.includes('#')) {
       window.scrollTo(0, 0);
     }
   }
--- src/ngmeta.types.ts
+++ src/ngmeta.types.ts
@@ -1,19 +1,103 @@
 /**
- * Interface for an the TagData interface.
+ * Interface for all meta details in head.
  */
-export type MetaData = {
-    type: string;
-    content: string;
-    attribute?: string;
+export type AllMeta = {
+  /**
+   * Document's title that is shown in a browser's title bar or a page's tab.
+   */
+  title?: string;
+  /**
+   * Description relating to the content of page.
+   */
+  description?: string;
+  /**
+   * Image URL for social media cards.
+   */
+  image?: string;
+  /**
+   * The canonical URL for your page.
+   */
+  canonical?: string;
+  /**
+   * The Twitter @username the card should be attributed to.
+   */
+  twitter?: string;
 }
 
 /**
- * Interface for an object that is used with the 'setHead()' function.
+ * Interface for all Facebook Open Graph meta details in head.
  */
-export type TagData = {
-    title?: string;
-    name?: MetaData[];
-    property?: MetaData[];
-    meta?: MetaData[];
-    canonical?: string;
+export type FacebookMeta = {
+  /**
+   * The locale of the resource.
+   */
+  locale?: string;
+  /**
+   * The type of media of your content.
+   */
+  type?: string;
+  /**
+   * The title of your article without any branding such as your site name.
+   */
+  title?: string;
+  /**
+   * A brief description of the content, usually between 2 and 4 sentences. This will displayed below the title of the post on Facebook.
+   */
+  description?: string;
+  /**
+   * The URL of the image that appears when someone shares the content to Facebook. See below for more info, and check out our best practices guide to learn how to specify a high quality preview image.
+   */
+  image?: string;
+  /**
+   * The canonical URL for your page.
+   */
+  url?: string;
+  /**
+   * In order to use Facebook Insights you must add the app ID to your page.
+   */
+  appId?: string;
+}
+
+/**
+ * Interface for all Google microdata meta details in head.
+ */
+export type GoogleMeta = {
+  /**
+   * Document's title that is shown in a browser's title bar or a page's tab.
+   */
+  title?: string;
+  /**
+   * Description relating to the content of page.
+   */
+  description?: string;
+  /**
+   * Image URL for social media cards.
+   */
+  image?: string;
+}
+
+/**
+ * Interface for all Google microdata meta details in head.
+ */
+export type TwitterMeta = {
+  /**
+   * A concise title for the related content.
+   */
+  title?: string;
+  /**
+   * A description that concisely summarizes the content as appropriate for presentation within a Tweet.
+   */
+  description?: string;
+  /**
+   * A URL to a unique image representing the content of the page.
+   */
+  image?: string;
+  /**
+   * A text description of the image conveying the essential nature of an image to users who are visually impaired.
+   */
+  alt?: string;
+  /**
+   * The Twitter @username the card should be attributed to.
+   */
+  site?: string;
 }
